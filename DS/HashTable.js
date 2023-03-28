01f637d081a04bd12c31c7bfd597f34525099f88
Varun Upadhya <varun.upadhya1@gmail.com>
2023-03-28 09:35:21 +0530

    Implementing graphs

--- DS/HashTable.js
+++ DS/HashTable.js
@@ -8,11 +8,44 @@ class HashTable {
   _hash(key) {
     let hash = 0;
     for (let i = 0; i < key.length; i++) {
-        //23 because to make it look even more random (some constant value)
+      //23 because to make it look even more random (some constant value)
       hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
     }
     return hash;
   }
 
-  set(key, value) {}
+  set(key, value) {
+    let index = this._hash(key);
+    let array = [key, value];
+    if (!this.dataMap[index]) {
+      this.dataMap[index] = [];
+    }
+
+    this.dataMap[index].push(array);
+  }
+
+  get(key) {
+    for (let i = 0; i < this.dataMap.length; i++) {
+      if (this.dataMap[i]) {
+        for (let j = 0; j < this.dataMap[i].length; j++) {
+          if (this.dataMap[i][j][0] === key) {
+            return this.dataMap[i][j][1];
+          }
+        }
+      }
+    }
+    return undefined;
+  }
+
+  keys() {
+    let allKeys = [];
+    for (let i = 0; i < this.dataMap.length; i++) {
+      if (this.dataMap[i]) {
+        for (let j = 0; j < this.dataMap[i].length; j++) {
+          allKeys.push(this.dataMap[i][j][0]);
+        }
+      }
+    }
+    return allKeys;
+  }
 }
