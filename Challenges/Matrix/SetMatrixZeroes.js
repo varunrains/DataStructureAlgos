ae8706ec90b1c5dac68334a77497c4960484cc30
Varun Upadhya <varun.upadhya1@gmail.com>
2023-05-01 15:50:06 +0530

    matrix problem

---
+++ Challenges/Matrix/SetMatrixZeroes.js
@@ -0,0 +1,14 @@
+var setZeroes = function(matrix) {
+    let m = matrix.length;
+    let n = matrix[0].length;
+
+    
+};
+
+/*
+Source: https://leetcode.com/problems/set-matrix-zeroes/
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+
+*/
