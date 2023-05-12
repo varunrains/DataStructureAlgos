072dad51a728cee59b3fa5c4d07aef1f5e22ad44
Varun Upadhya <varun.upadhya1@gmail.com>
2023-05-12 09:45:12 +0530

    Linked list duplicate removal

---
+++ Challenges/LinkedList/RemoveDuplicates.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let curr = head;
+ 
+   while (curr !== null && curr.next !== null) {
+     if (curr.val === curr.next.val) {
+       curr.next = curr.next.next;
+     } else {
+       curr = curr.next;
+     }
+   }
+ 
+   return head;
+ };
+
+/*
+Source: https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+Input: head = [1,1,2]
+Output: [1,2]
+*/
