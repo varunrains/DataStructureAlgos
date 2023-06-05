5fa06c841baaf6df2c3ba9548b95601611cda87f
Varun Upadhya <varun.upadhya1@gmail.com>
2023-06-05 23:52:37 +0530

    maxium area of island

--- Challenges/Graphs/MaxNumberOfIslands.js
+++ Challenges/Graphs/MaxNumberOfIslands.js
@@ -46,5 +46,5 @@ Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,
 Output: 6
 Explanation: The answer is not 11, because the island must be connected 4-directionally.
 
-
+Maxium area
 */
