84ece5f51439f045aeb87d6680613d7e6e18fcfa
Varun Upadhya <varun.upadhya1@gmail.com>
2024-06-08 14:24:15 +0530

    2D arrays few more problems

--- Scaler/Arrays/PrefixSum/ConsoleApp1/2DArray.cs
+++ Scaler/Arrays/PrefixSum/ConsoleApp1/2DArray.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,12 +35,64 @@ namespace ArraysDSA
                 for (int col = 0; col < A[0].Count; col++)
                 {
 
-                    //Column wise sum
+                    //Row wise sum
                     sum += A[row][col];
                 }
                 list.Add(sum);
             }
             return list;
+        }
+
+        public static List<List<int>> MatrixTranspose(List<List<int>> A)
+        {
+            List<List<int>> result = new List<List<int>>();
+
+            for(int col=0;col < A[0].Count; col++)
+            {
+                List<int> subArray = new List<int>();
+                for(int row = 0; row < A.Count; row++)
+                {
+                    subArray.Add(A[row][col]);
+                }
+                result.Add(subArray);
+            }
+
+            return result;
+
+
+        }
+
+        public static void RotateMatrixBy90(List<List<int>> a)
+        {
+            for(int row=0;row < a.Count; row++)
+            {
+              
+               for(int col = 0; col < a[0].Count; col++)
+                {
+                    if(row < col)
+                    {
+                        var temp = a[col][row];
+                        a[col][row] = a[row][col];
+                        a[row][col] = temp;
+                    }
+                }
+            }
+        }
+
+            public static List<List<int>> PrintAntiDiagonals(List<List<int>> A)
+        {
+            int i = 0;
+            int j= A[0].Count;
+            List<int> rowItems = new List<int>();
+            for(int row=0;row < A.Count; row++)
+            {
+                for(int col = 0; col < A[0].Count; col++)
+                {
+
+                }
+            }
+
+            return null;
         }
 
         public static int MainDiagnolSum(List<List<int>> A)
