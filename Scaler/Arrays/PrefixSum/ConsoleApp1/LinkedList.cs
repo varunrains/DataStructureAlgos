159a18ffab434aa374dd318a9048ff43e1f2ed2a
Varun Upadhya <varun.upadhya1@gmail.com>
2024-09-08 15:50:36 +0530

    Deleting an item from linked list

--- Scaler/Arrays/PrefixSum/ConsoleApp1/LinkedList.cs
+++ Scaler/Arrays/PrefixSum/ConsoleApp1/LinkedList.cs
@@ -47,5 +47,60 @@ namespace ArraysDSA
 
             return A;
         }
+
+
+        //https://www.scaler.com/academy/mentee-dashboard/class/236106/assignment/problems/30667
+        public ListNode DeleteTheNodeBasedOnIndex(ListNode A, int B)
+        {
+            var temp = A;
+
+            if (B == 0)
+            {
+                A = temp.next;
+                return A;
+            }
+
+
+            for(int i = 1; i <= B - 1; i++)
+            {
+                if (temp.next?.next != null)
+                {
+                    temp = temp.next;
+                }
+                else
+                {
+                    temp.next = null;
+                    return A;
+                }
+            }
+
+            var toConnect = temp.next?.next;
+            temp.next = toConnect;
+            return A;
+        }
+
+        //https://www.scaler.com/academy/mentee-dashboard/class/236106/assignment/problems/40?navref=cl_tt_lst_nm
+        public ListNode ReverseLinkedList(ListNode A)
+        {
+            var curr = A;
+            ListNode prev = null;
+
+            if(curr.next == null)
+            {
+                return A;
+            }
+
+            //curr = curr.next;
+            while (curr != null)
+            {
+                var nextNode = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = nextNode;
+            }
+            A = prev;
+            return A;
+
+        }
     }
 }
