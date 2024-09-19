b956d482b56184f26c0ae158f9420e3540eb93c0
Varun Upadhya <varun.upadhya1@gmail.com>
2024-09-19 10:19:32 +0530

    maximum and minimum subarray calculation using stack

--- Scaler/Arrays/PrefixSum/ConsoleApp1/Stacks.cs
+++ Scaler/Arrays/PrefixSum/ConsoleApp1/Stacks.cs
@@ -187,6 +187,139 @@ namespace ArraysDSA
         }
 
 
+        //https://www.scaler.com/academy/mentee-dashboard/class/236120/assignment/problems/7042
+        public static int MaxAndMinInSubarrays(List<int> A)
+        {
+            //First we need to find the nearest greater to left and right
+            //nearest smaller to left and right
+            //Apply contribution technique after that
+            //You will get sum of all maximum elements and minimum
+            //and then do max - min
+
+            var nsl = new int[A.Count];
+            var nsr = new int[A.Count];
+
+            Stack<int> nsl_stack = new Stack<int>();
+            Stack<int> nsr_stack = new Stack<int>();  
+
+            nsl[0] = -1;
+            nsr[A.Count - 1] = A.Count; //Index should be outside the array size
+
+            nsl_stack.Push(0);
+            nsr_stack.Push(A.Count-1);
+
+            //nearest smallest in the left
+            for (int i = 1; i < A.Count; i++)
+            {
+                while(nsl_stack.Count > 0 && A[nsl_stack.Peek()] >= A[i])
+                {
+                    nsl_stack.Pop();
+                }
+
+                if(nsl_stack.Count == 0)
+                {
+                    nsl[i] = -1;
+                }
+                else
+                {
+                    nsl[i] = nsl_stack.Peek();
+                }
+
+                nsl_stack.Push(i);
+
+            }
+
+
+            //nearest smallest from the right
+            for (int i = A.Count - 2; i >= 0; i--)
+            {
+                while (nsr_stack.Count > 0 && A[nsr_stack.Peek()] >= A[i])
+                {
+                    nsr_stack.Pop();
+                }
+
+                if (nsr_stack.Count == 0)
+                {
+                    nsr[i] = A.Count;
+                }
+                else
+                {
+                    nsr[i] = nsr_stack.Peek();
+                }
+
+                nsr_stack.Push(i);
+            }
+            
+            var ngl = new int[A.Count];
+            var ngr = new int[A.Count];
+
+            ngl[0] = -1;
+            ngr[A.Count - 1] = A.Count;
+
+            Stack<int> ngl_stack = new Stack<int>();
+            Stack<int> ngr_stack = new Stack<int>();
+
+            ngl_stack.Push(0);
+            ngr_stack.Push(A.Count-1);
+
+            //nearest greater in the left
+            for (int i = 1; i < A.Count; i++)
+            {
+                while (ngl_stack.Count > 0 && A[ngl_stack.Peek()] <= A[i])
+                {
+                    ngl_stack.Pop();
+                }
+
+                if (ngl_stack.Count == 0)
+                {
+                    ngl[i] = -1;
+                }
+                else
+                {
+                    ngl[i] = ngl_stack.Peek();
+                }
+
+                ngl_stack.Push(i);
+
+            }
+
+
+            //nearest greater from the right
+            for (int i = A.Count - 2; i >= 0; i--)
+            {
+                while (ngr_stack.Count > 0 && A[ngr_stack.Peek()] <= A[i])
+                {
+                    ngr_stack.Pop();
+                }
+
+                if (ngr_stack.Count == 0)
+                {
+                    ngr[i] = A.Count;
+                }
+                else
+                {
+                    ngr[i] = ngr_stack.Peek();
+                }
+
+                ngr_stack.Push(i);
+            }
+
+            //Sum of all sub-arrays where ith element is sub-array is maximum
+            long f1 = 0;
+
+            //sum of all sub-arrays where ith element is sub-array is minimum
+            long f2 = 0;
+
+            for (int i = 0; i < A.Count; i++)
+            {
+                f1 = (f1 + ((long)(i - ngl[i]) * (ngr[i] - i) % 1000000007) * A[i]) % 1000000007;
+                f2 = (f2 + ((long)(i - nsl[i]) * (nsr[i] - i) % 1000000007) * A[i]) % 1000000007;
+            }
+
+
+            return (int)((f1 - f2) + 1000000007) % 1000000007;
+        }
+
         private static int EvaluateTheExpression(int op1, int op2, string op)
         {
             if (op == "+")
