438f259521c62304aa44d7c692bcc68c83752ee4
Varun Upadhya <varun.upadhya1@gmail.com>
2024-09-20 09:38:04 +0530

    Double ended queue implementation start

--- Scaler/Arrays/PrefixSum/ConsoleApp1/UserQueues.cs
+++ Scaler/Arrays/PrefixSum/ConsoleApp1/UserQueues.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DequeNet;
+
 
 namespace ArraysDSA
 {
@@ -73,4 +76,91 @@ namespace ArraysDSA
      * int param3 = obj.peek();
      * boolean param4 = obj.empty();
      */
+
+
+    //https://www.scaler.com/academy/mentee-dashboard/class/236124/assignment/problems/50?navref=cl_tt_lst_nm
+
+    public static class QueueProblems
+    {
+        public static List<int> SlidingMaximum(List<int> A, int B)
+        {
+           Deque<int> s = new Deque<int>();
+            var res = new List<int>();
+            var maximum = int.MinValue;
+            var maxIndex = 0;
+            for(int i = 0; i < B; i++)
+            {
+               // maximum = Math.Max(maximum, A[i]);
+                if(maximum < A[i])
+                {
+                    maximum = A[i];
+                    maxIndex = i;
+                }
+            }
+            res.Add(A[maxIndex]);
+            
+            s.PushRight(maxIndex);
+
+            return null;
+             
+                              
+        }
+    }
+
+    //Need to review the below code
+    public class DoubleEndedQueue
+    {
+        private Stack<int> stack;
+        private Queue<int> queue;
+        public DoubleEndedQueue()
+        {
+              stack = new Stack<int>();
+              queue = new Queue<int>();
+        }
+
+
+        public void InsertFront(int x)
+        {
+          queue.Enqueue(x);
+        }
+
+        public void InsertBack(int x)
+        {
+            stack.Push(x);
+        }
+
+        public int RemoveFromFront()
+        {
+            return queue.Dequeue();
+        }
+
+        public int RemoveFromBack()
+        {
+            return stack.Pop();
+        }
+
+        public bool IsFrontEmpty()
+        {
+            return queue.Count == 0;
+        }
+
+        public bool IsBackEmpty()
+        {
+            return stack.Count == 0;
+        }
+
+        public int PeekFront()
+        {
+            if (queue.Count == 0) throw new Exception("Queue Empty");
+            return queue.Peek();
+        }
+
+        public int PeekBack()
+        {
+            if(stack.Count == 0) throw new Exception("Stack Empty");
+            return stack.Peek();
+        }
+
+    }
+
 }
