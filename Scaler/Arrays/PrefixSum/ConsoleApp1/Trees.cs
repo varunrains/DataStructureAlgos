daead83a100c64728d49069092d4f282846946a8
Varun Upadhya <varun.upadhya1@gmail.com>
2024-09-23 09:16:28 +0530

    Tree problems if the tree has the sum

--- Scaler/Arrays/PrefixSum/ConsoleApp1/Trees.cs
+++ Scaler/Arrays/PrefixSum/ConsoleApp1/Trees.cs
@@ -35,5 +35,70 @@ namespace ArraysDSA
 
             return list;
         }
+
+        //https://www.scaler.com/academy/mentee-dashboard/class/236128/assignment/problems/234?navref=cl_tt_lst_nm
+        public static int HasPathSum(TreeNode A, int B)
+        {
+
+            return HasPathSumRec(A, B) ? 1 : 0;
+
+        }
+
+        private static bool HasPathSumRec(TreeNode A, int remainingSum)
+        {
+            if (A == null) { return false; }
+
+            remainingSum = remainingSum - A.val;
+
+            if (remainingSum == 0 && A.left == null && A.right == null)
+            {
+                return true;
+            }
+
+            bool leftPathSum = A.left != null && HasPathSumRec(A.left, remainingSum);
+            bool rightPathSum = A.right != null && HasPathSumRec(A.right, remainingSum);
+
+            return leftPathSum || rightPathSum;
+        }
+
+
+        public static int EqualTreePartition(TreeNode A)
+        {
+           long sum = SumOfTree(A);
+           
+            if(sum % 2 == 1) return 0;
+
+            var ds = CheckIfWeCanHavePartition(A, sum/2);
+            if (ds == true)
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+        private static bool CheckIfWeCanHavePartition(TreeNode A, long target)
+        {
+
+            if(A == null) return false;
+            long leftSum = SumOfTree(A.left);
+            long rightSum = SumOfTree(A.right);
+
+            if (leftSum == target || rightSum == target)
+            {
+                return true;
+            }
+
+            return (CheckIfWeCanHavePartition(A.left, target) || CheckIfWeCanHavePartition(A.right,target));
+        }
+
+
+        private static long SumOfTree(TreeNode A)
+        {
+            if(A == null) { return 0; }
+
+            long totalSum = A.val +  SumOfTree(A.left) + SumOfTree(A.right);
+
+            return totalSum;
+        }
     }
 }
